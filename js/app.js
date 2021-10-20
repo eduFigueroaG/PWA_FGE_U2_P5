a8645d658d6297a94e5d747790b3974b20b2b44b
eduFigueroaG <20183ti135@utez.edu.mx>
2021-10-19 22:02:39 -0500

    app changes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
     navigator.serviceWorker.register("/sw.js");
 }*/
 let url = window.location.href;
-let swDirect = "/PWA-FGE-U2-P5/sw.js";
+let swDirect = "/PWA_FGE_U2_P5/sw.js";
 
 if (navigator.serviceWorker) {
     if (url.includes("localhost")) {
